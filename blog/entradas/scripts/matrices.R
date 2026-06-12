a1d8893365719454b3691af356d68c3647a16dff
lemendozaru <mendozaruiz.luisenrique@gmail.com>
2026-06-11 19:28:53 -0600

    Entrada sobre matrices terminada

--- blog/entradas/scripts/matrices.R
+++ blog/entradas/scripts/matrices.R
@@ -43,9 +43,6 @@ taquilla_precuelas <- matrix(c(474.5, 552.5, 310.7, 338.7, 380.3, 468.5),
 nombres_precuelas <- c("The Phantom Menace", "Attack of the Clones",
                        "Revenge of the Sith")
 
-# Nombramos las columnas
-colnames(taquilla_precuelas) <- region
-
 # Nombramos las filas
 rownames(taquilla_precuelas) <- nombres_precuelas
 
@@ -53,4 +50,48 @@ rownames(taquilla_precuelas) <- nombres_precuelas
 taquilla_todas <- rbind(taquilla, taquilla_precuelas)
 
 # Imprimimos la matriz final
+# taquilla_todas
+
+# Calculemos el total de taquilla por región
+# taquilla_region <- colSums(taquilla_todas)
+
+# Mostremos el resultado
+# taquilla_region
+
+# Mostremos solo la taquilla fuera de los EU
+# taquilla_todas[,2]
+
+# Calculemos solo la media de esa taquilla
+# mean(taquilla_todas[,2])
+
+# Seleccionemos solo la taquilla de las primeras dos
+# taquilla_todas[1:2,2]
+
+# Calculemos su media
+# mean(taquilla_todas[1:2,2])
+
+# Para calcular la asistencia
+# dividamos entre 5
+# asistentes <- taquilla_todas / 5
+
+# Mostremos el resultado
+# asistentes
+
+# Creamos la matriz con los precios
+# precios <- matrix(c(5,5,6,6,7,7,4,4,4.5,4.5,4.9,4.9),
+#                  byrow = TRUE, nrow = 6)
+
+# Dividamos la primera entre la segunda
+# asistentes_reales <- taquilla_todas / precios
+
+# Mostremos los asistentes reales
+# asistentes_reales
+
+# Calculemos y agreguemos la suma de columnas
+taquilla_todas <- rbind(taquilla_todas, colSums(taquilla_todas))
+
+# Ahora calculemos la suma de filas
+taquilla_todas <- cbind(taquilla_todas, rowSums(taquilla_todas))
+
+# Mostremos
 taquilla_todas
