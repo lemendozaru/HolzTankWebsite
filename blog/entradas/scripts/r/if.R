3cd64d4012d18dbdedea2269c45c2d75f97926f8
lemendozaru <mendozaruiz.luisenrique@gmail.com>
2026-06-15 20:46:07 -0600

    Prácticas de sistemas embebidos agregadas

---
+++ blog/entradas/scripts/r/if.R
@@ -0,0 +1,11 @@
+# Declaramos la variable
+x <- 5
+
+# Creamos la condicional
+if (x < 0) {
+  print("x es negativa")
+} else if (x == 0) {
+  print("x es cero")
+} else {
+  print("x es positiva")
+}
